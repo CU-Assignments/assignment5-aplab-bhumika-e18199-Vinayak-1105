bd03c25a6c65bc50a6ee81c55b0e0cdef50b931c
Vinayak-1105 <sharmavinayak116@gmail.com>
2025-03-10 19:54:08 +0530

    098. Validate Binary Search Tree.cpp

--- 098. Validate Binary Search Tree.cpp
+++ 098. Validate Binary Search Tree.cpp
@@ -1,25 +1,3 @@
-98. Validate Binary Search Tree
-Given a binary tree, determine if it is a valid binary search tree (BST).
-
-Assume a BST is defined as follows:
-
-The left subtree of a node contains only nodes with keys less than the node's key.
-The right subtree of a node contains only nodes with keys greater than the node's key.
-Both the left and right subtrees must also be binary search trees.
-Example 1:
-    2
-   / \
-  1   3
-Binary tree [2,1,3], return true.
-Example 2:
-    1
-   / \
-  2   3
-Binary tree [1,2,3], return false.
-
-题目大意：给一个二叉树，判断这个二叉树是不是合法的二叉搜索树～
-分析：既然是二叉搜索树，那么按照左根右遍历后的结果一定是增序～所以只需要中序遍历一遍，判断遍历结果的数组是不是后面数一定大于前面数就可以了～
-
 
 class Solution {
 private:
