5969fc5c0eb107dae4951e5ff450d849921fe04e
Vinayak-1105 <sharmavinayak116@gmail.com>
2025-03-10 19:56:39 +0530

    116. Populating Next Right Pointers in Each Node.cpp

--- 116. Populating Next Right Pointers in Each Node.cpp
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -1,24 +1,4 @@
-//BFS with queue
-//Runtime: 20 ms, faster than 94.93% of C++ online submissions for Populating Next Right Pointers in Each Node.
-//Memory Usage: 17.5 MB, less than 16.36% of C++ online submissions for Populating Next Right Pointers in Each Node.
-//time: O(N), space: O(N)
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
 
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
 
 class Solution {
 public:
